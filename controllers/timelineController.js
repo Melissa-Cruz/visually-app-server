186f878dbf7f80382f9004e235f2d6e4600e37f6
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-06-07 20:37:21 -0400

    added  try and catch to timelineControllers

--- controllers/timelineController.js
+++ controllers/timelineController.js
@@ -1,37 +1,48 @@
+const Timeline = require("../models/timelineModel");
 
-
-
-  
 const getAllTimelines = async (req, res, next) => {
-     
+  try {
+    const timelines = await Timeline.find({});
+
     return res.status(200).json({
       success: { message: "This route gets all the timelines." },
+      data: {timelines:timelines},
+      statusCode: 200,
     });
-  };
-  
-const getTimeline = async (req, res, next) => {
-    return res.status(200).json({
-      success: { message: "This will send a single timeline by its id." },
-    });
+  } catch (error) {
+    return next(error);
+  }
 };
-  
-const createTimeline =async (req, res, next) => {
-    return res.status(200).json({
-      success: { message: "This will create a new timeline." },
-    });
-  };
-  
+
+const getTimeline = async (req, res, next) => {
+
+  return res.status(200).json({
+    success: { message: "This will send a single timeline by its id." },
+  });
+};
+
+const createTimeline = async (req, res, next) => {
+  return res.status(200).json({
+    success: { message: "This will create a new timeline." },
+  });
+};
+
 const updateTimeline = async (req, res, next) => {
-    return res.status(200).json({
-      success: { message: "This will update a timeline by its id." },
-    });
-  };
-  
-const deleteTimeline =async (req, res, next) => {
-    return res.status(200).json({
-      success: { message: "This will delete a timeline by its id" },
-    });
-  };
+  return res.status(200).json({
+    success: { message: "This will update a timeline by its id." },
+  });
+};
 
+const deleteTimeline = async (req, res, next) => {
+  return res.status(200).json({
+    success: { message: "This will delete a timeline by its id" },
+  });
+};
 
-  module.exports = {getAllTimelines, getTimeline, createTimeline, updateTimeline, deleteTimeline};
+module.exports = {
+  getAllTimelines,
+  getTimeline,
+  createTimeline,
+  updateTimeline,
+  deleteTimeline,
+};
